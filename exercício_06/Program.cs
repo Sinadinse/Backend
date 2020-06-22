841cdb49d48c394110905132ae3642e22b6f44dd
Treinamento <Treinamento@UNI173.tecnologiaunica.local>
2020-06-22 11:56:33 -0300

    versão 0.1

---
+++ exercício_06/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exercício_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float ht, vh, pd, sb, td, sl;
+
+            Console.WriteLine("Horas trabalhadas");
+            ht = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Valor da hora");
+            vh = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Percentagem de desconto");
+            pd = float.Parse(Console.ReadLine());
+
+            sb = ht * vh;
+            td = (pd / 100) * sb;
+            sl = sb - td;
+
+
+            Console.WriteLine();
+            Console.WriteLine("Salário líquido");
+            Console.WriteLine(sl.ToString("f2"));
+
+            Console.ReadKey();
+        }
+    }
+}
